d61ccc4e6719ebaa0aabaa9a6fe1c845b675f40e
Mehul <mehul.kaklotar@gmail.com>
2017-04-05 19:21:43 +0530

    nonce interface & abstract nonce class

---
+++ src/mehulkaklotar/wpnonce/class-nonce-abstract.php
@@ -0,0 +1,143 @@
+<?php
+/**
+ * NonceAbstract
+ * abstract class
+ *
+ * @package MehulKaklotarWPNonce
+ * @license GPL2+
+ */
+
+namespace mehulkaklotar\wpnonce;
+
+
+/**
+ * Class NonceAbstract
+ * @package MehulKaklotarWPNonce
+ */
+class NonceAbstract implements NonceInterface {
+
+	/**
+	 * The name of the action
+	 *
+	 * @var string
+	 **/
+	private $action = '';
+	/**
+	 * The name of the request
+	 *
+	 * @var string
+	 **/
+	private $request_name = '';
+	/**
+	 * The nonce
+	 *
+	 * @var string
+	 **/
+	private $nonce = '';
+	/**
+	 * The lifetime of a nonce in seconds
+	 *
+	 * @var int
+	 **/
+	private $lifetime = DAY_IN_SECONDS;
+
+	/**
+	 * Set the action
+	 *
+	 * @param string $action The action name.
+	 *
+	 * @return string $action     The action.
+	 **/
+	public function setAction( string $action ) {
+		$this->action = $action;
+
+		return $this->getAction();
+	}
+
+	/**
+	 * Get the action
+	 *
+	 * @return string $action The action.
+	 **/
+	public function getAction() {
+		return $this->action;
+	}
+
+	/**
+	 * Set the request name
+	 *
+	 * @param string $request_name The new request name.
+	 *
+	 * @return string $request_name    The request name.
+	 **/
+	public function setRequestName( string $request_name ) {
+		$this->request_name = $request_name;
+
+		return $this->getRequestName();
+	}
+
+	/**
+	 * Get the request name
+	 *
+	 * @return string $request_name The request name.
+	 **/
+	public function getRequestName() {
+		return $this->request_name;
+	}
+
+	/**
+	 * Set the lifetime
+	 *
+	 * @param int $lifetime The new lifetime.
+	 *
+	 * @return int $lifetime    The current lifetime.
+	 **/
+	public function setLifetime( int $lifetime ) {
+		$this->lifetime = $lifetime;
+
+		return $this->getLifetime();
+	}
+
+	/**
+	 * Get the lifetime
+	 *
+	 * @param boolean $actual_lifetime Whether to use the lifetime used by WordPress or the lifetime stored in $lifetime. Optional. Default: true.
+	 *
+	 * @return int $lifetime       The lifetime.
+	 **/
+	public function getLifetime( bool $actual_lifetime = true ) {
+		if ( $actual_lifetime ) {
+			/**
+			 * We run $lifetime through the 'nonce_life' to get the actual lifetime, which
+			 * the system is using right now, since other systems might interfere with
+			 * this filter.
+			 */
+			return apply_filters( 'nonce_life', $this->lifetime );
+		}
+
+		return $this->lifetime;
+	}
+
+	/**
+	 * Set the nonce
+	 *
+	 * @param string $nonce The nonce to verify.
+	 *
+	 * @return string $nonce    The nonce
+	 **/
+	public function setNonce( string $nonce ) {
+		$this->nonce = $nonce;
+
+		return $this->getNonce();
+	}
+
+	/**
+	 * Get the nonce
+	 *
+	 * @return string $nonce The nonce
+	 **/
+	public function getNonce() {
+		return $this->nonce;
+	}
+
+}
---
+++ src/mehulkaklotar/wpnonce/interface-wp-nonce.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * NonceInterface
+ * The interface of the Nonce OOP.
+ *
+ * @package MehulKaklotarWPNonce
+ * @license GPL2+
+ */
+
+namespace mehulkaklotar\wpnonce;
+
+/**
+ * Nonce Interface
+ *
+ * @package MehulKaklotarWPNonce
+ * @license GPL2+
+ */
+
+
+interface NonceInterface {
+
+    /**
+     * Set an action
+     *
+     * @param string $action The action
+     *
+     * @return string $action
+     */
+    public function setAction( string $action );
+
+    /**
+     * Get the action
+     *
+     * @return string $action The action
+     **/
+    public function getAction();
+
+    /**
+     * Set a request name
+     *
+     * @param string $request_name The request name
+     *
+     * @return string $request The request
+     **/
+    public function setRequestName( string $request_name );
+
+    /**
+     * Get the request name
+     *
+     * @return string $request The request name
+     **/
+    public function getRequestName();
+
+    /**
+     * Set a lifetime
+     *
+     * @param int $lifetime The lifetime.
+     *
+     * @return int $lifetime The lifetime
+     **/
+    public function setLifetime( int $lifetime );
+
+    /**
+     * Get the lifetime
+     *
+     * @param  boolean $actual_lifetime Whether to run the 'nonce_life' filter or not. Optional. Default is true.
+     *
+     * @return int     $lifetime     The lifetime
+     **/
+    public function getLifetime( bool $actual_lifetime = true );
+
+    /**
+     * Set the nonce
+     *
+     * @param string $nonce The nonce to verify.
+     *
+     * @return string $nonce     The nonce
+     **/
+    public function setNonce( string $nonce );
+
+    /**
+     * Get the nonce
+     *
+     * @return string $nonce The nonce
+     **/
+    public function getNonce();
+}
